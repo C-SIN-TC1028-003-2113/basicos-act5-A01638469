0d09f231b90d932b981957eec2116827e1ae408a
A01638469 <A01638469@itesm.mx>
2021-08-18 19:21:19 +0000

    act_clase_5 practica

--- assignments/00Escalera/src/exercise.py
+++ assignments/00Escalera/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
-    pass
+    import math
+
+    altura = float(input('Altura de la casa: '))
+    angulo = int(input('Angulo en grados: '))
+    largo = round(altura / math.sin(math.radians(angulo)))
+    print('Largo de la escalera: ' + str(largo))
 
 if __name__ == '__main__':
     main()
